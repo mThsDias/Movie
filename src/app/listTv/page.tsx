f9d8a004d90c1331e411fe9fbdb5596a63f185e7
Matheus Dias <matheusdiasmello@icloud.com>
2023-12-12 17:46:12 -0300

    style: check page Tv

--- src/app/listTv/page.tsx
+++ src/app/listTv/page.tsx
@@ -1,13 +1,100 @@
 "use client";
 
+import React, { useEffect, useState } from "react";
+import { ImageComponent } from "@/components/Image";
+import * as S from "../listMovie/styles";
+import { TitleComponent } from "@/components/Title";
+import { DateComponent } from "@/components/Date";
+import { formatDate } from "@/functions";
+import Link from "next/link";
+import { Footer } from "@/components/Footer";
 import { NavBar } from "@/components/Header/NavBar";
-import React from "react";
+import { Tv } from "@/Tv/context/types";
 
 export default function ListTv() {
+  const [movieList, setMovieList] = React.useState<Tv[]>([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
+  const [loading, setLoading] = React.useState<boolean>(false);
+  const [error, setError] = React.useState<string | null>(null);
+
+  const apiKey = "dcf6fe444e49bcbe4d8f215076000be9";
+  const api = `https://api.themoviedb.org/3/tv/popular?api_key=${apiKey}&language=en-US&page=`;
+  useEffect(() => {
+    setLoading(true);
+
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`${api}${currentPage}`);
+        const json = await response.json();
+
+        if (response?.ok) {
+          setMovieList(json.results);
+          setTotalPages(json.total_pages);
+        } else {
+          throw new Error(json.message);
+        }
+      } catch (error: unknown) {
+        setError(error as string);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [currentPage]);
+
+  const handleNextPage = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  const handlePrevPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  if (loading) return <div>Loading...</div>;
+
+  if (error) return <div>{error}</div>;
+
   return (
-    <main style={{ background: "#0f0f0f", height: "100vh" }}>
+    <main style={{ background: "#0f0f0f" }}>
       <NavBar />
-      <h1>ListSerie</h1>
+      <S.ContainerGrid>
+        {movieList.map((item) => (
+          <S.BoxGrid key={item.id}>
+            <ImageComponent
+              img={item.poster_path}
+              h={350}
+              w={230}
+              borderRadius={10}
+            />
+            <Link href={`/tv/${item.id}`}>
+              <h2>
+                <TitleComponent title={item} />
+              </h2>
+            </Link>
+            <p>
+              <DateComponent
+                date={item.first_air_date || item.release_date}
+                formatDate={formatDate}
+              />
+            </p>
+          </S.BoxGrid>
+        ))}
+      </S.ContainerGrid>
+      <S.BoxButton>
+        <button onClick={handlePrevPage} disabled={currentPage === 1}>
+          Página Anterior
+        </button>
+        <button onClick={handleNextPage} disabled={currentPage === totalPages}>
+          Próxima Página
+        </button>
+      </S.BoxButton>
+      <Footer />
     </main>
   );
 }
